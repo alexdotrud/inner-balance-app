d5f7af9102e38017b169a44edddebf233e8cfe37
alexdotrud <rudenko530@gmail.com>
2025-08-01 22:04:51 +0200

    Add the gunicorn package

--- inner_balance/urls.py
+++ inner_balance/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tracker.views import my_tracker
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("tracker/", my_tracker, name='tracker'),
 ]
