1f5b910cd4adab936737152cf39a39472798cbc2
alexdotrud <rudenko530@gmail.com>
2025-08-01 22:19:01 +0200

    Install dj-database-url package:

--- inner_balance/settings.py
+++ inner_balance/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t(xr^vlg=b4+qzx!gk^s#2cq_i5zqxnv@^)&gtqp5vlh&&nih*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
