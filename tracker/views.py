e1990cf4aed08cd9491719db9ee20351218b9d7e
alexdotrud <rudenko530@gmail.com>
2025-08-02 13:52:59 +0200

    Add top level templates directory and basic html structure to it

--- tracker/views.py
+++ tracker/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Tasks
+from django.views import generic
+from .models import Task
 
 # Create your views here.
-def my_tracker(request):
-    user_tasks = Tasks.objects.filter(user_id=request.user)
-    context = {
-        'tasks': user_tasks,
-    }
-    return render(request, 'tracker/my_tracker.html', context)
+class TasksList(generic.ListView):
+    model = Task
+    context_object_name = 'tasks'
