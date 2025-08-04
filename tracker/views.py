a17fea1d156f96026102798e79aeb08d270be446
alexdotrud <rudenko530@gmail.com>
2025-08-04 18:14:15 +0200

    Add get and post functions to TAsk model

--- tracker/views.py
+++ tracker/views.py
@@ -1,10 +1,28 @@
 from django.views.generic import TemplateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
+from .forms import TaskForm
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'tracker/dashboard.html'
+    
+    # Sending data to template
+    def get_context_data(self, **kwargs):
+        tasks = Task.objects.filter(user=self.request.user)
+        return {
+            'tasks': tasks,
+            'task_count': tasks.count(),
+            'task_form': TaskForm(),
+        }
+    # If form submitted -redirects to dashboard.
+    def post(self, request):
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+        return redirect('tracker:dashboard')
 
 class TaskListView(ListView):
     model = Task
