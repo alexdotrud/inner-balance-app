39e36c120d4c96c66e6c7ea567c9b797515f107e
alexdotrud <rudenko530@gmail.com>
2025-08-04 16:57:11 +0200

    Add save method and a checkbox to Task model

--- tracker/models.py
+++ tracker/models.py
@@ -9,3 +9,14 @@ class Task(models.Model):
     description = models.TextField(blank=True)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'title') # users can have similar task name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f"{self.user.username}-{self.title}") # automatically generates a slug
+        super().save(*args, **kwargs) 
+        
+    def __str__(self):
+        return f"{self.title} ({'✓' if self.is_completed else '✗'})"
