25d22c626c91a66b5e8dc23983de2de0c9b88593
alexdotrud <rudenko530@gmail.com>
2025-08-12 11:54:02 +0200

    Chabfe userProfile model, using ValueValidators

--- profiles/models.py
+++ profiles/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.timezone import now
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 User = get_user_model()
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     last_reset = models.DateField(default=now)
-    water_intake = models.FloatField(default=0)
-    sleep_hours = models.FloatField(default=0)
-    water_goal = models.FloatField(default=8.0)
-    sleep_goal = models.FloatField(default=8.0)
+    water_intake = models.FloatField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(20)]
+    )
+    sleep_hours = models.FloatField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(20)]
+    )
+    
+    water_goal = models.FloatField(
+        default=8.0,
+        validators=[MinValueValidator(1), MaxValueValidator(20)]
+    )
+    sleep_goal = models.FloatField(
+        default=8.0,
+        validators=[MinValueValidator(1), MaxValueValidator(20)]
+    )
     description = models.TextField(blank=True, default="")
 
     class Meta:
